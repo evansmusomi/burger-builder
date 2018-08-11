eb9ba386bededa8bf918dbd2dff6b6a00b162a03
Evans Musomi <evans.musomi@gmail.com>
2018-08-11 06:38:06 +0300

    Makes Navigation Mobile Responsive (#8)
    * feat: makes responsive adjustments to nav items
    * feat: adds a backdrop to the side drawer and logic to close it
    * feat: adds a hamburger icon that toggles the side drawer
    * chore: adds a summary of current features in the readme

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 import classes from "./Toolbar.css";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
