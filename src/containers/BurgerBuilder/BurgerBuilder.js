5eafa6d1ab45b1873f44dae6f184f442b9a09eb1
Evans Musomi <evans.musomi@gmail.com>
2018-08-16 08:16:30 +0300

    feat: adds contact data form in the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,15 +25,6 @@ class BurgerBuilder extends Component {
     loading: false,
     error: false
   };
-  
-  componentDidMount(){
-    axios.get("https://burger-builder-260c7.firebaseio.com/ingredients.json")
-      .then(response => {
-        this.setState({ ingredients: response.data})
-      }).catch(error => {
-        this.setState({ error: true })
-      })
-  }
 
   componentDidMount() {
     axios
