8264404f1562d9676e64ddab53bd3484cd73dd3e
Evans Musomi <evans.musomi@gmail.com>
2018-09-03 07:52:12 +0300

    feat: handles auth actions via redux

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
+import authReducer from "./store/reducers/auth";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import registerServiceWorker from "./registerServiceWorker";
@@ -17,7 +18,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
+  order: orderReducer,
+  auth: authReducer
 });
 
 const store = createStore(
